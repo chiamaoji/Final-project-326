03f602aebb3ba1545b21bf0d96035570e380cb4a
chiamaoji <165223392+chiamaoji@users.noreply.github.com>
2024-04-08 21:51:28 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+
+"""
+Main entry point of the bias detection program.
+"""
+
+import bias_detection
+
+def main():
+    # main program  here
+    pass
+
+if __name__ == "__main__":
+    main()
